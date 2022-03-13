563170026549ee5a2f0d510bfc940ed3191ddc28
Pavel Pak <talkin.watermelon@gmail.com>
2022-03-13 23:11:07 +0900

    Added modals

--- src/js/script.js
+++ src/js/script.js
@@ -13,12 +13,14 @@ $(document).ready(function() {
         }]
     });
 
+    //Tabs toggler
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
         $(this)
             .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
             .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
 
+    //Content toggler
     // $('.catalog-item__link').each(function(i) {
     //     $(this).on('click', function(e) {
     //         e.preventDefault();
@@ -36,6 +38,7 @@ $(document).ready(function() {
     // });
 
     //Optimized version of toggle function above
+
     function toggleSlide(item) {
         $(item).each(function(i) {
             $(this).on('click', function(e) {
@@ -48,4 +51,22 @@ $(document).ready(function() {
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
+
+    //Modal
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+    });
+
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        });
+    })
+
 });
